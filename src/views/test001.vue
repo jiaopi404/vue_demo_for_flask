5911405710a554b38fa37db36bd40227ed75fac5
jiaopi404 <1240659804@qq.com>
2020-05-07 09:34:58 +0800

    update 添加注释, 帮助理解代码

--- src/views/test001.vue
+++ src/views/test001.vue
@@ -17,24 +17,29 @@ export default {
   name: 'test001',
   data () {
     return {
-      myData: [],
-      tmpData: [],
-      count: 0
+      myData: [], // 渲染的数据
+      tmpData: [], // 暂存下一页的数据
+      count: 0 // 请求的次数, < 2 时禁用无限滚动, 防止无限滚动函数绑定时即执行函数
     }
   },
   mounted () {
+    // 开局执行两次向借口请求数据, page = 1 和 page = 2
+    // 第一次直接渲染, 第二次存于内存中
     this.getMoreData()
     this.getMoreData()
   },
   methods: {
     getMoreData () {
+      // 模拟请求借口
       setTimeout(() => {
         this.count++
+
         const arr = []
         for (let i = 0; i < 20; i++) {
           arr.push(Math.floor(Math.random() * 1000))
         }
         console.log('get more data: ', arr)
+
         if (this.myData.length === 0) {
           this.myData.push(...arr)
         } else {
@@ -44,6 +49,9 @@ export default {
     },
     pushData () {
       console.log('get data function')
+      // 1. 内存中数据加入到真实数据中
+      // 2. 置空内存数据
+      // 3. 请求更多的数据
       this.myData.push(...this.tmpData)
       this.tmpData = []
       this.getMoreData()
